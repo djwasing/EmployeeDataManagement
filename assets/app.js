fdd3f2dadbc62d40e48a96fcdd892b7fb3268496
mango dyl <dylanbonds@hotmail.com>
2019-01-08 19:33:37 -0600

    updated js

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,42 @@
 
 
-var employeeRef = ref().child("employees/");
 
-function newEmployeeData(employeeName, role, startDate, monthlyRate) {
-   database.employeeRef.push({
 
-       name: employeeName,
-       role: role,
-       startDate: startDate,
-       monthlyRate: monthlyRate,
-       dateAdded: firebase.database.ServerValue.TIMESTAMP
+$("#submit").on("click", function() {
 
-   })
-}
+    var employeeRef = ref().child("employees/");
+    var newName = $('#employee-name').val().trim();
+    var newRole = $('#employee-role').val().trim();
+    var newDate = $('#employee-startYear').val().trim();
+    var newRate = parseInt($('#employee-rate').val().trim());
 
-var database = firebase.database();
+    function newEmployeeData(employeeName, role, startDate, monthlyRate) {
+        database.employeeRef.push({
+
+            name: employeeName,
+            role: role,
+            startDate: startDate,
+            monthlyRate: monthlyRate,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+        });
+    };
+
+    newEmployeeData(newName, newRole, newDate, newRate);
+
+    $("#employee-name").val("");
+    $("#employee-role").val("");
+    $("#employee-startYear").val("");
+    $("#employee-rate").val("");
+
+});
+    
+
 
 database.ref().on("child_added", function (snapshot) {
 
     console.log(snapshot);
- })
+
+    
+
+})
